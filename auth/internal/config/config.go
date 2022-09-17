e525dbf06906e12640d3b050616a25891019efa9
udholdenhed <nazarslota03@gmail.com>
2022-09-17 23:06:41 +0300

    Added configs for development and production.

--- auth/internal/config/config.go
+++ auth/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"sync"
 	"time"
 
@@ -47,11 +48,23 @@ var (
 func C() *Config {
 	once.Do(func() {
 		v := viper.New()
-		v.SetConfigName("app")
 		v.AddConfigPath("configs")
+		if os.Getenv("ENVIRONMENT") == "DEVELOPMENT" {
+			v.SetConfigName("development")
+			if err := v.ReadInConfig(); err != nil {
+				panic(err)
+			}
+		} else {
+			v.AutomaticEnv()
+			v.SetConfigName("production")
 
-		if err := v.ReadInConfig(); err != nil {
-			panic(err)
+			bindEnvPostgreSQL(v)
+			bindEnvMongoDB(v)
+			bindEnvRedis(v)
+
+			if err := v.ReadInConfig(); err != nil {
+				panic(err)
+			}
 		}
 
 		if err := v.Unmarshal(&instance); err != nil {
@@ -63,4 +76,27 @@ func C() *Config {
 		}
 	})
 	return instance
+}
+
+func bindEnvPostgreSQL(v *viper.Viper) {
+	_ = v.BindEnv("POSTGRESQL_HOST")
+	_ = v.BindEnv("POSTGRESQL_PORT")
+	_ = v.BindEnv("POSTGRESQL_USERNAME")
+	_ = v.BindEnv("POSTGRESQL_PASSWORD")
+	_ = v.BindEnv("POSTGRESQL_DBNAME")
+	_ = v.BindEnv("POSTGRESQL_SSLMODE")
+}
+
+func bindEnvMongoDB(v *viper.Viper) {
+	_ = v.BindEnv("MONGODB_HOST")
+	_ = v.BindEnv("MONGODB_PORT")
+	_ = v.BindEnv("MONGODB_USERNAME")
+	_ = v.BindEnv("MONGODB_PASSWORD")
+	_ = v.BindEnv("MONGODB_DATABASE")
+}
+
+func bindEnvRedis(v *viper.Viper) {
+	_ = v.BindEnv("REDIS_ADDR")
+	_ = v.BindEnv("REDIS_PASSWORD")
+	_ = v.BindEnv("REDIS_DB")
 }
