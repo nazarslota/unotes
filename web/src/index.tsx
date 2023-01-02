0a525c37cb4f3ebd602c1fac047bf983441f2b5d
udholdenhed <nazarslota03@gmail.com>
2023-01-03 01:16:55 +0200

    .gitignore update. Refactoring.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -16,4 +15,3 @@ root.render(
     </React.StrictMode>
 );
 
-reportWebVitals();
