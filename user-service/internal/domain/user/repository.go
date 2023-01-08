7aeb6f776981383c26b78694726d09604995420d
udholdenhed <nazarslota03@gmail.com>
2023-01-08 17:48:20 +0200

    The user service has been deleted.

--- user-service/internal/domain/user/repository.go
+++
@@ -1,9 +0,0 @@
-package user
-
-import "context"
-
-type Repository interface {
-	Create(ctx context.Context, u User) error
-	FindByID(ctx context.Context, id string) (*User, error)
-	FindByUsername(ctx context.Context, username string) (*User, error)
-}
--- user-service/internal/domain/user/user.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	ID           string `json:"id" bson:"_id" db:"id"`
-	Username     string `json:"username" bson:"username" db:"username"`
-	Email        string `json:"email" bson:"email" db:"email"`
-	PasswordHash string `json:"password_hash" bson:"password_hash" db:"password_hash"`
-}
