a5c4c053f6a748a55af841e390ff8a79570f7c27
udholdenhed <nazarslota03@gmail.com>
2023-01-25 22:30:57 +0200

    UpdateNote implemented.

---
+++ note/internal/service/note/updatenote.go
@@ -0,0 +1,49 @@
+package note
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	domainnote "github.com/nazarslota/unotes/note/internal/domain/note"
+)
+
+type UpdateNoteRequest struct {
+	ID         string
+	NewTitle   string
+	NewContent string
+}
+
+type UpdateNoteResponse struct {
+}
+
+type UpdateNoteRequestHandler interface {
+	Handle(ctx context.Context, request *UpdateNoteRequest) (*UpdateNoteResponse, error)
+}
+
+type updateNoteRequestHandler struct {
+	NoteRepository domainnote.Repository
+}
+
+var ErrUpdateNoteNotFound = func() error { return domainnote.ErrNoteNotFound }()
+
+func NewUpdateNoteRequestHandler(noteRepository domainnote.Repository) UpdateNoteRequestHandler {
+	return &updateNoteRequestHandler{
+		NoteRepository: noteRepository,
+	}
+}
+
+func (h updateNoteRequestHandler) Handle(ctx context.Context, request *UpdateNoteRequest) (*UpdateNoteResponse, error) {
+	note := domainnote.Note{
+		ID:      request.ID,
+		Title:   request.NewTitle,
+		Content: request.NewContent,
+	}
+
+	if err := h.NoteRepository.UpdateOne(ctx, note); errors.Is(err, domainnote.ErrNoteNotFound) {
+		return nil, fmt.Errorf("failed to update note: %w", ErrUpdateNoteNotFound)
+	} else if err != nil {
+		return nil, fmt.Errorf("failed to update note: %w", err)
+	}
+	return &UpdateNoteResponse{}, nil
+}
