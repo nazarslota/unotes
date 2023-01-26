7b90280f4ed47a903753f21f0f719396c38cff9a
udholdenhed <nazarslota03@gmail.com>
2023-01-26 22:18:59 +0200

    FindManyAsync added to Repository.

---
+++ note/internal/service/note/getnotesasync.go
@@ -0,0 +1,34 @@
+package note
+
+import (
+	"context"
+
+	domainnote "github.com/nazarslota/unotes/note/internal/domain/note"
+)
+
+type GetNotesAsyncRequest struct {
+	UserID string
+}
+
+type GetNotesAsyncResponse struct {
+	Notes <-chan domainnote.Note
+}
+
+type GetNotesAsyncRequestHandler interface {
+	Handle(ctx context.Context, request GetNotesAsyncRequest) (GetNotesAsyncResponse, <-chan error)
+}
+
+type getNotesAsyncRequestHandler struct {
+	NoteRepository domainnote.Repository
+}
+
+var ErrGetNotesAsyncNotFound = func() error { return domainnote.ErrNoteNotFound }()
+
+func NewGetNotesAsyncRequestHandler(noteRepository domainnote.Repository) GetNotesAsyncRequestHandler {
+	return &getNotesAsyncRequestHandler{NoteRepository: noteRepository}
+}
+
+func (h getNotesAsyncRequestHandler) Handle(ctx context.Context, request GetNotesAsyncRequest) (GetNotesAsyncResponse, <-chan error) {
+	nts, errs := h.NoteRepository.FindManyAsync(ctx, request.UserID)
+	return GetNotesAsyncResponse{Notes: nts}, errs
+}
