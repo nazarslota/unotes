f3b8c82b870a28ab6453515b7dd3ea693731a6c4
udholdenhed <nazarslota03@gmail.com>
2023-01-30 15:43:18 +0200

    Rewritten tests, improved documentation of the utils package. The code of some functions has been optimized.

--- auth/pkg/utils/utils.go
+++ auth/pkg/utils/utils.go
@@ -1,18 +1,27 @@
 package utils
 
 import (
+	"errors"
 	"net"
 	"net/url"
 	"os"
 	"os/signal"
 )
 
-// GracefulShutdown returns a channel(<-chan struct{}) that closes when an attempt is made to terminate a program.
+var (
+	// ErrBuildMongoURIInvalidHost represents an error returned by the BuildMongoURI function when the invalid host
+	// is used.
+	ErrBuildMongoURIInvalidHost = errors.New("invalid host")
+)
+
+// GracefulShutdown creates a channel for signaling a graceful shutdown.
+// When either an os.Interrupt or os.Kill signal is received, the returned channel is closed.
+// This channel can be used by other parts of the program to wait for the shutdown signal.
 func GracefulShutdown() <-chan struct{} {
 	shutdown := make(chan struct{})
 	go func() {
 		s := make(chan os.Signal, 1)
-		signal.Notify(s, os.Interrupt)
+		signal.Notify(s, os.Interrupt, os.Kill)
 
 		<-s
 		close(shutdown)
@@ -20,11 +29,15 @@ func GracefulShutdown() <-chan struct{} {
 	return shutdown
 }
 
-// BuildMongoURI builds new MongoDB URI with the given parameters.
+// BuildMongoURI builds a MongoDB connection URI from the provided host, port, username, and password.
+// The returned URI string is of the format: "mongodb://[username:password@]host:port".
+//
+// If the port is not provided, the scheme of the URI is set to "mongodb+srv" instead of "mongodb".
+// In addition, use of the +srv connection string modifier automatically sets the tls (or the equivalent ssl) option to
+// true for the connection.
 func BuildMongoURI(host, port, username, password string) (string, error) {
-	proto := "mongodb"
-	if len(port) == 0 {
-		proto += "+srv"
+	if len(host) == 0 {
+		return "", ErrBuildMongoURIInvalidHost
 	}
 
 	var userinfo *url.Userinfo
@@ -34,14 +47,13 @@ func BuildMongoURI(host, port, username, password string) (string, error) {
 		userinfo = url.User(username)
 	}
 
+	proto := "mongodb"
 	if len(port) > 0 {
 		host = net.JoinHostPort(host, port)
+	} else {
+		proto += "+srv"
 	}
+	uri := url.URL{Scheme: proto, User: userinfo, Host: host}
 
-	uri := url.URL{
-		Scheme: proto,
-		User:   userinfo,
-		Host:   host,
-	}
 	return uri.String(), nil
 }
--- auth/pkg/utils/utils_test.go
+++ auth/pkg/utils/utils_test.go
@@ -1,123 +1,78 @@
 package utils
 
 import (
-	"os"
-	"os/exec"
+	"syscall"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGracefulShutdown(t *testing.T) {
-	// Test that the shutdown channel is closed when the program is interrupted
-	cmd := exec.Command(os.Args[0], "-test.run=TestGracefulShutdownHelper")
-	err := cmd.Start()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	time.Sleep(time.Second)
-	if err := cmd.Process.Signal(os.Interrupt); err != nil {
-		t.Fatal(err)
-	}
-
-	err = cmd.Wait()
-	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-		return
-	}
-	t.Fatalf("process ran with err %v, want exit status 1", err)
-}
-
-func TestGracefulShutdownHelper(t *testing.T) {
 	shutdown := GracefulShutdown()
+	assert.NotNil(t, shutdown)
+
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+
+		err := syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		require.NoError(t, err)
+	}()
+
 	select {
+	case <-time.After(15 * time.Second):
+		require.Fail(t, "Timed out waiting for shutdown")
 	case <-shutdown:
-		t.Error("GracefulShutdown returned immediately")
-	default:
-	}
-	<-time.After(2 * time.Second)
-	select {
-	case <-shutdown:
-		t.Error("GracefulShutdown returned after 2s")
-	default:
 	}
 }
 
 func TestBuildMongoURI(t *testing.T) {
 	tests := []struct {
-		name    string
-		host    string
-		port    string
-		user    string
-		pass    string
-		want    string
-		wanterr error
+		host     string
+		port     string
+		username string
+		password string
+		expected string
+		err      error
 	}{
 		{
-			name:    "basic URI",
-			host:    "localhost",
-			port:    "27017",
-			user:    "",
-			pass:    "",
-			want:    "mongodb://localhost:27017",
-			wanterr: nil,
+			host:     "localhost",
+			port:     "27017",
+			username: "",
+			password: "",
+			expected: "mongodb://localhost:27017",
+			err:      nil,
 		},
 		{
-			name:    "URI with username and password",
-			host:    "localhost",
-			port:    "27017",
-			user:    "user",
-			pass:    "pass",
-			want:    "mongodb://user:pass@localhost:27017",
-			wanterr: nil,
+			host:     "cluster0.mongodb.net",
+			port:     "",
+			username: "user",
+			password: "pass",
+			expected: "mongodb+srv://user:pass@cluster0.mongodb.net",
+			err:      nil,
 		},
 		{
-			name:    "URI with username only",
-			host:    "localhost",
-			port:    "27017",
-			user:    "user",
-			pass:    "",
-			want:    "mongodb://user@localhost:27017",
-			wanterr: nil,
+			host:     "localhost",
+			port:     "",
+			username: "user",
+			password: "",
+			expected: "mongodb+srv://user@localhost",
+			err:      nil,
 		},
 		{
-			name:    "URI with SRV scheme",
-			host:    "localhost",
-			port:    "",
-			user:    "",
-			pass:    "",
-			want:    "mongodb+srv://localhost",
-			wanterr: nil,
-		},
-		{
-			name:    "URI with SRV scheme and username",
-			host:    "localhost",
-			port:    "",
-			user:    "user",
-			pass:    "",
-			want:    "mongodb+srv://user@localhost",
-			wanterr: nil,
-		},
-		{
-			name:    "URI with SRV scheme and username and password",
-			host:    "localhost",
-			port:    "",
-			user:    "user",
-			pass:    "pass",
-			want:    "mongodb+srv://user:pass@localhost",
-			wanterr: nil,
+			host:     "",
+			port:     "",
+			username: "",
+			password: "",
+			expected: "",
+			err:      ErrBuildMongoURIInvalidHost,
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := BuildMongoURI(tt.host, tt.port, tt.user, tt.pass)
-			if err != tt.wanterr {
-				t.Errorf("BuildMongoURI() error = %v, wantErr %v", err, tt.wanterr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("BuildMongoURI() = %v, want %v", got, tt.want)
-			}
-		})
+	for _, test := range tests {
+		uri, err := BuildMongoURI(test.host, test.port, test.username, test.password)
+		assert.Equal(t, test.err, err)
+		assert.Equal(t, test.expected, uri)
 	}
 }
