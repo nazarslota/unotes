ece4f249d0c86fa965e28d69ec871bb4ad13dedd
nazarslota <nazarslota03@gmail.com>
2023-03-13 23:18:55 +0200

    mongo: tests and documentation updated

--- note/internal/storage/mongo/mongo_test.go
+++ note/internal/storage/mongo/mongo_test.go
@@ -5,41 +5,76 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestNewMongoDB(t *testing.T) {
-	database, err := NewMongoDB(context.Background(), Config{
-		Host:     "localhost",
-		Port:     "27017",
-		Database: "testdb",
-	})
-	assert.NoError(t, err)
-
-	err = database.Drop(context.Background())
-	require.NoError(t, err)
-
-	database, err = NewMongoDB(context.Background(), Config{
-		Host:     "",
-		Port:     "",
-		Database: "testdb",
-	})
-	assert.Error(t, err)
-
-	database, err = NewMongoDB(context.Background(), Config{
-		Host:     "localhost",
-		Port:     "-1",
-		Database: "testdb",
-	})
-	assert.Error(t, err)
-
-	ctx, cancel := context.WithCancel(context.Background())
+	canceled, cancel := context.WithCancel(context.Background())
 	cancel()
 
-	database, err = NewMongoDB(ctx, Config{
-		Host:     "localhost",
-		Port:     "27017",
-		Database: "testdb",
-	})
-	assert.ErrorIs(t, err, context.Canceled)
+	tests := []struct {
+		name     string
+		ctx      context.Context
+		host     string
+		port     string
+		username string
+		password string
+		database string
+		wantErr  bool
+	}{
+		{
+			name:     "should return new mongo database client",
+			ctx:      context.Background(),
+			host:     "localhost",
+			port:     "27017",
+			username: "",
+			password: "",
+			wantErr:  false,
+		},
+		{
+			name:     "should return an error if host is invalid",
+			ctx:      context.Background(),
+			host:     "invalid-host",
+			port:     "27017",
+			username: "",
+			password: "",
+			wantErr:  true,
+		},
+		{
+			name:     "should return an error if port is invalid",
+			ctx:      context.Background(),
+			host:     "localhost",
+			port:     "-1",
+			username: "",
+			password: "",
+			wantErr:  true,
+		},
+		{
+			name:     "should return an error if context is invalid",
+			ctx:      canceled,
+			host:     "localhost",
+			port:     "27017",
+			username: "",
+			password: "",
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, err := NewMongoDB(tt.ctx, Config{
+				Host:     tt.host,
+				Port:     tt.port,
+				Username: tt.username,
+				Password: tt.password,
+				Database: tt.database,
+			})
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Nil(t, db)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, db)
+			}
+		})
+	}
 }
