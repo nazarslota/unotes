edb72d97fdacd7160fe66dd5389bab86d673f2b7
nazarslota <nazarslota03@gmail.com>
2023-05-05 14:40:42 +0300

    note: dependencies updated

--- note/api/proto/note.pb.gw.go
+++ note/api/proto/note.pb.gw.go
@@ -339,7 +339,7 @@ func RegisterNoteServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 // RegisterNoteServiceHandlerFromEndpoint is same as RegisterNoteServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterNoteServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
